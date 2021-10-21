f69b1a750b4b448f48652522c85d557c019b6b51
jose <josejota11@gmail.com>
2021-10-21 20:37:33 +0200

    Se añade la funcionalidad de subir 2 imagenes 2.0 y mejora del entorno visual_v1

--- app/src/main/java/com/example/famdif_final/MySuggestionsFragment.java
+++ app/src/main/java/com/example/famdif_final/MySuggestionsFragment.java
@@ -7,12 +7,10 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import androidx.annotation.NonNull;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -41,48 +39,25 @@ public class MySuggestionsFragment extends BaseFragment {
         listaSugerencias.setAdapter(adapter);
 
         getMainActivity().getSupportActionBar().setTitle("MIS SUGERENCIAS");
-        getSugerencias(view);
+        recuperarSugerencias(view);
 
         return view;
     }
-    /*
-    private void recuperarSugerencias(View view){
-        //MainActivity.db.collection("suggestions").document(MainActivity.mAuth.getCurrentUser().getEmail().toString())
-        MainActivity.db.collection("suggestions").whereEqualTo("autor",MainActivity.mAuth.getCurrentUser().getEmail().toString()).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+
+    private void recuperarSugerencias(View view) {
+        MainActivity.db.collection("suggestions").whereEqualTo("autor", MainActivity.mAuth.getCurrentUser().getEmail().toString()).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(Task<QuerySnapshot> task) {
-                for(QueryDocumentSnapshot doc : task.getResult()){
-                    Log.i(TAG, doc.getId() + " => " + doc.getData());
-                    Log.i(TAG, "El título de la sugerencia es: "+doc.getString("titulo"));
-                    titulosSugerencias.add("-Titulo: "+doc.getString("titulo").toString()+"\n"+"\n"
-                            +"-Autor: "+doc.getString("autor")+"\n"+"\n"
-                            +"-Detalles: "+doc.getString("cuerpo"));
+                for (QueryDocumentSnapshot doc : task.getResult()) {
+                    titulosSugerencias.add("-Titulo: " + doc.getString("titulo").toString() + "\n" + "\n"
+                            + "-Autor: " + doc.getString("autor") + "\n" + "\n"
+                            + "-Detalles: " + doc.getString("cuerpo"));
                     adapter.notifyDataSetChanged();
                 }
 
             }
-        });*/
-
-
-        private void getSugerencias(View view){
-            Query q=MainActivity.databaseReference.child("suggestions").orderByChild("autor").equalTo(MainActivity.mAuth.getCurrentUser().getEmail().toString());
-            q.addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot snapshot) {
-                    for(DataSnapshot dato: snapshot.getChildren()){
-                        titulosSugerencias.add("Titulo: "+dato.child("titulo").getValue().toString()+"\n"+"\n"
-                                +"-Autor: "+dato.child("autor").getValue().toString()+"\n"+"\n"
-                                +"-Detalles: "+dato.child("cuerpo").getValue().toString()+"\n"+"\n");
-                        adapter.notifyDataSetChanged();
-                    }
-                }
-
-                @Override
-                public void onCancelled(@NonNull DatabaseError error) {
-
-                }
-            });
-        }
+        });
+    }
 
 
 }
